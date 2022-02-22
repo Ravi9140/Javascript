4efc638eb14ed2c21c306e8df9d89053a62292b9
Ravi9140 <78776196+Ravi9140@users.noreply.github.com>
2022-02-22 10:42:51 +0530

    Create LoneOnes.js

---
+++ LoneOnes.js
@@ -0,0 +1,26 @@
+function countLoneOnes(user_input)
+{
+    var remainder=0,previous_remainder=0,count=0;
+    while(user_input>0)
+    {
+        previous_remainder=remainder;
+        remainder=user_input%10;
+        user_input=Math.floor(user_input/10);
+
+        //2nd Condition: For case where first digit is of Number is 1 and second digit is not 1
+        if((previous_remainder==1 && remainder!=1) || (user_input==0 && remainder==1 && previous_remainder!=1))
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+var LoneOnes1=countLoneOnes(101);
+console.log(`\n101 -> ${LoneOnes1}`);
+
+var LoneOnes2=countLoneOnes(1191);
+console.log(`\n1191 -> ${LoneOnes2}`);
+
+var LoneOnes3=countLoneOnes(10101);
+console.log(`\n10101 -> ${LoneOnes3}`);
