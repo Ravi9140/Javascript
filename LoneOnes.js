7f3ae82186767d15927378b58967a272c415d7ad
Ravi9140 <78776196+Ravi9140@users.noreply.github.com>
2022-03-03 10:22:56 +0530

    Update LoneOnes.js

--- LoneOnes.js
+++ LoneOnes.js
@@ -1,17 +1,18 @@
 function countLoneOnes(user_input)
 {
-    var remainder=0,previous_remainder=0,count=0;
+    var remainder=0,previous_remainder=0,count=0,prev_prev_rem=0;
     while(user_input>0)
     {
-        previous_remainder=remainder;
-        remainder=user_input%10;
-        user_input=Math.floor(user_input/10);
+      prev_prev_rem=previous_remainder;
+      previous_remainder=remainder;
+      remainder=user_input%10;
+      user_input=Math.floor(user_input/10);
 
         //2nd Condition: For case where first digit is of Number is 1 and second digit is not 1
-        if((previous_remainder==1 && remainder!=1) || (user_input==0 && remainder==1 && previous_remainder!=1))
-        {
+      if((previous_remainder==1 && remainder!=1 && prev_prev_rem!=1) || (user_input==0 && remainder==1 && previous_remainder!=1))
+      {
             count++;
-        }
+      }
     }
     return count;
 }
@@ -24,3 +25,6 @@ console.log(`\n1191 -> ${LoneOnes2}`);
 
 var LoneOnes3=countLoneOnes(10101);
 console.log(`\n10101 -> ${LoneOnes3}`);
+
+var LoneOnes4=countLoneOnes(1011101011);
+console.log(`\n1011101011 -> ${LoneOnes4}`);
